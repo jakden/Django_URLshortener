c2049d1217fc0a6a9ae8d576c7df73a41df33074
Jake <jakd.dennis@gmail.com>
2016-04-27 03:16:31 +0000

    CSS Changes

--- minihtml/models.py
+++ minihtml/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.utils import timezone
 from django.template.defaultfilters import slugify
+from django.forms.widgets import TextInput
+from django import forms
 # Create your models here.
 
 class Post(models.Model):
@@ -13,8 +15,6 @@ class Post(models.Model):
 			default=timezone.now)
 	tiny = models.CharField(
 			max_length=2000)
-	
-
 
 	def __str__(self):
 		return self.website
